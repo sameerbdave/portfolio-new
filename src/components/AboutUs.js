5480d3223f9fae2788fed5863fed0fa534aa2206
Sameer Dave <sameerbdave@gmail.com>
2022-09-16 18:27:43 +0530

    about-details

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -3,7 +3,9 @@ import React from "react";
 const AboutUs =() =>{
     return(
         <div>
-            <h1>About Us</h1>
+            <h1>Hey This is Sameer Dave</h1>
+            <h2>Senior Front End Developer </h2>
+            <p>I am a professional Senior Front End Developer specializing in modern techniques, best practice for web standards and open source technologies. I am looking for an opportunity in which I can utilize my skills and experience in a qualitative environment, where my knowledge and experience can get a scope for improvement and where I can contribute my maximum ideas and efforts to the organization.</p>
         </div>
     )
 }
